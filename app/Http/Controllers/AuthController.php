cf0868bf17f9e36147a6a15607fb77dde0a97a0b
Md. Mahdi Hasan <96673899+mahdihasan28@users.noreply.github.com>
2024-06-11 02:40:04 +0600

    Project Setup & Authenticate in progress

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $validatedData = $request->validate([
+            'name' => 'required|string|max:255',
+            'mobile' => 'required|string|max:255|unique:users',
+            'password' => 'required|string|min:8',
+        ]);
+
+        $user = User::create([
+            'name' => $validatedData['name'],
+            'mobile' => $validatedData['mobile'],
+            'password' => Hash::make($validatedData['password']),
+        ]);
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json(['access_token' => $token, 'token_type' => 'Bearer']);
+    }
+
+    public function login(Request $request)
+    {
+        $credentials = $request->validate([
+            'mobile' => 'required|string',
+            'password' => 'required|string',
+        ]);
+
+        if (!Auth::attempt($credentials)) {
+            return response()->json(['message' => 'Invalid login details'], 401);
+        }
+
+        $user = Auth::user();
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json(['access_token' => $token, 'token_type' => 'Bearer']);
+    }
+
+    public function checkMobile(Request $request)
+    {
+        $request->validate([
+            'mobile' => 'required|string', // You may add additional validation rules here
+        ]);
+
+        $mobile = $request->input('mobile');
+
+        // Check if the mobile number exists in the database
+        $isRegistered = User::where('mobile', $mobile)->exists();
+
+        return response()->json(['isRegistered' => $isRegistered]);
+    }
+}
