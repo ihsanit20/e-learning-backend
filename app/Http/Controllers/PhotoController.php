039482f0da5778275eb75157616410bfd69350e0
msisaif <msi132043@gmail.com>
2024-08-23 17:19:14 +0600

    upload-photo option with remove old photo

--- app/Http/Controllers/PhotoController.php
+++ app/Http/Controllers/PhotoController.php
@@ -28,17 +28,32 @@ class PhotoController extends Controller
     
         $model_instance = $options[$request->option]->findOrFail($request->id);
 
-        $path = $request->file('photo')->store('mentor', 's3');
+        $directoryPath = $request->option . '/' . date('Y') . '/' . $model_instance->id;
+
+        $filename = uniqid() . '.' . $request->file('photo')->getClientOriginalExtension();
+
+        $path = $request->file('photo')->storeAs($directoryPath, $filename, 's3');
 
         $s3Url = Storage::disk('s3')->url($path);
-
+        
+        $old_photo_url = $model_instance->photo;
+        
         $model_instance->photo = $s3Url;
 
         $model_instance->save();
 
+        if ($old_photo_url) {
+            $old_photo_path = parse_url($old_photo_url, PHP_URL_PATH);
+            $old_photo_path = ltrim($old_photo_path, '/');
+    
+            if (Storage::disk('s3')->exists($old_photo_path)) {
+                Storage::disk('s3')->delete($old_photo_path);
+            }
+        }
+
         return response()->json([
             'message' => 'Photo uploaded successfully',
-            'path' => $s3Url
+            'path' => $s3Url,
         ], 200);
     }
 }
