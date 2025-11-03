0e84aca8c511f6e8f7a78767595b4d842040db02
msisaif <msi132043@gmail.com>
2025-11-03 23:01:13 +0600

    feat: add phone attribute setter to format phone numbers with country code

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Str;
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
@@ -35,6 +36,15 @@ class User extends Authenticatable
     {
         return Attribute::make(
             get: fn ($value) => $value ?? ('https://ui-avatars.com/api/?name=' . str_replace(' ', '+', $this->name)),
+        );
+    }
+
+    protected function phone(): Attribute
+    {
+        return Attribute::make(
+            set: fn ($value) => Str::startsWith($value, '+88')
+                ? $value
+                : '+88' . $value
         );
     }
 
